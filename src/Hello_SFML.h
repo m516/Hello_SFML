6899f56d22fac6c1adf774ec19e42acd9b1c0691
Micah <micah112358@gmail.com>
2020-01-16 23:24:31 -0600

    Create a working SFML project

---
+++ src/Hello_SFML.h
@@ -0,0 +1,8 @@
+﻿// Hello_SFML.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
